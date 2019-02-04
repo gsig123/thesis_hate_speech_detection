1f33397a942ba096b6ec9cb1f67e64500fd841da
gsig123 <gis14@hi.is>
2019-02-04 14:56:32 +0100

    Project Setup, datasets from OffensEval2019

---
+++ src/classifiers/classifier_base.py
@@ -0,0 +1,12 @@
+class Classifier: 
+    def __init__(self):
+        "TODO"
+
+    def train(self):
+        "TODO"
+
+    def predict(self):
+        "TODO"
+    
+    def confusion_matrix(self):
+        "TODO"
